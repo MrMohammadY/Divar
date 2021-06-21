b7e4c424c82d2d12384f8d992e7aed9bbbe10217
MrMohammadY <m.yazdani000@gmail.com>
2021-06-21 20:39:22 +0430

    Chore(accounts): fix parameter custom user manager and make migrations

--- accounts/models.py
+++ accounts/models.py
@@ -7,22 +7,22 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class CustomUserManager(UserManager):
-    def _create_user(self, phone, password, **extra_fields):
+    def _create_user(self, phone_number, password, **extra_fields):
 
-        if not phone:
+        if not phone_number:
             raise ValueError('The given phone must be set')
 
-        user = self.model(phone=phone, **extra_fields)
+        user = self.model(phone_number=phone_number, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, phone, password=None, **extra_fields):
+    def create_user(self, phone_number, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(phone, password, **extra_fields)
+        return self._create_user(phone_number, password, **extra_fields)
 
-    def create_superuser(self, phone, password=None, **extra_fields):
+    def create_superuser(self, phone_number, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -31,7 +31,7 @@ class CustomUserManager(UserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(phone, password, **extra_fields)
+        return self._create_user(phone_number, password, **extra_fields)
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
