0c356ea28cdd22b9a85f12bf36bde25fde25628f
MrMohammadY <m.yazdani000@gmail.com>
2021-06-23 13:02:23 +0430

    Chore(detail view): create a detail view

--- advertisement/urls.py
+++ advertisement/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import advertisement_list
+from .views import advertisement_list, advertisement_detail
 
 app_name = 'advertisement'
 
 urlpatterns = [
     path('', advertisement_list, name='advertisement_list'),
+    path('detail/<int:uuid>/<slug:slug>/', advertisement_detail, name='advertisement_detail'),
 ]
--- advertisement/views.py
+++ advertisement/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from advertisement.models import Advertisement
 
@@ -6,3 +6,10 @@ from advertisement.models import Advertisement
 def advertisement_list(request):
     advertisements = Advertisement.objects.all()
     return render(request, template_name='advertisement/list.html', context={'advertisements': advertisements})
+
+
+def advertisement_detail(request, uuid, slug):
+    advertisement = get_object_or_404(Advertisement, uuid=uuid, slug=slug)
+    return render(request, template_name='advertisement/detail.html', context={'advertisement': advertisement})
+
+
