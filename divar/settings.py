88aa41a2ed200c76bd3016d91bce4c81598aef7a
MrMohammadY <m.yazdani000@gmail.com>
2021-06-24 15:46:33 +0430

    Chore(divar): fix the setting and url

--- divar/settings.py
+++ divar/settings.py
@@ -114,9 +114,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
