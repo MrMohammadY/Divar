6c87764192428cb4830e8458cb8f5b9133a6b45e
MrMohammadY <m.yazdani000@gmail.com>
2021-06-29 19:06:12 +0430

    Update(view): update view for categories

--- advertisement/views.py
+++ advertisement/views.py
@@ -1,7 +1,8 @@
 from django.db.models import Q
 from django.views.generic import ListView, DetailView
+from django.http import Http404
 
-from advertisement.models import Advertisement
+from advertisement.models import Advertisement, Category
 
 
 # def advertisement_list(request):
@@ -20,15 +21,23 @@ class AdvertisementListView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(object_list=None, **kwargs)
+
         if self.kwargs.get('category'):
+            category = Category.objects.filter(
+                Q(slug=self.kwargs['category']) | Q(parent__slug=self.kwargs['category'])
+            ).first()
 
-            context['advertisements'] = Advertisement.objects.select_related('category').filter(
-                Q(category__slug=self.kwargs['category']) | Q(category__parent__slug=self.kwargs['category'])
+            if category is None:
+                raise Http404()
 
+            context['advertisements'] = Advertisement.objects.filter(
+                Q(category__slug=category.slug) | Q(category__parent__slug=category.slug)
             )
 
         else:
             context['advertisements'] = Advertisement.objects.all()
+
+        context['categories'] = Category.objects.filter(parent=None)
 
         return context
 
